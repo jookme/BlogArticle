2338d26cab0c5c8207840c4885ee30e741c04ff5
jookme <37918552+jookme@users.noreply.github.com>
2018-04-02 20:21:01 +0800

    Add files via upload

--- models/article.go
+++ models/article.go
@@ -1,8 +1,10 @@
 package models
 
 import (
-	db "BlogArticle/database"
+	"database/sql"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //定义一个文章结构体类型，成员数据包括：Id、标题、作者、内容和最后一次修改时间
@@ -18,9 +20,14 @@ type Article struct {
 //传入参数：无
 //传出参数：文章列表信息，错误信息
 func GetArticleList() ([]string, error) {
+	// SqlDb := Database.Init()
+	SqlDb, err := sql.Open("mysql", "root:1996@tcp(127.0.0.1:3306)/blog")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer SqlDb.Close()
 	//从表Article中查询文章列表
-	rows, err := db.SqlDb.Query("SELECT Title FROM Article")
-
+	rows, err := SqlDb.Query("SELECT Title FROM Article")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -47,10 +54,16 @@ func GetArticleList() ([]string, error) {
 //传入参数：文章id
 //传出参数：对应文章内容，错误信息
 func GetArtiContent(id int) (string, error) {
+	// SqlDb := db.Init()
+	SqlDb, err := sql.Open("mysql", "root:1996@tcp(127.0.0.1:3306)/blog")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer SqlDb.Close()
 	//用content存放获取的文章内容
 	var content string
 	//从表Article中查询对应标题title的文章内容
-	err := db.SqlDb.QueryRow("SELECT Content FROM Article where Id = ?", id).Scan(&content)
+	err = SqlDb.QueryRow("SELECT Content FROM Article where Id = ?", id).Scan(&content)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,8 +75,14 @@ func GetArtiContent(id int) (string, error) {
 //传入参数：Article 结构体类型变量
 //传出参数：Article表中影响的数据行数，错误信息
 func UpdateArticle(article Article) (ra int64, err error) {
+	// SqlDb := db.Init()
+	SqlDb, err := sql.Open("mysql", "root:1996@tcp(127.0.0.1:3306)/blog")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer SqlDb.Close()
 	// PreparedStatement 可以防止SQL注入攻击
-	stmt, err := db.SqlDb.Prepare("UPDATE Article SET Title=?, Author=? , Content=? , LastTime=? WHERE Id=?")
+	stmt, err := SqlDb.Prepare("UPDATE Article SET Title=?, Author=? , Content=? , LastTime=? WHERE Id=?")
 	defer stmt.Close()
 	if err != nil {
 		log.Fatalln(err)
@@ -85,12 +104,18 @@ func UpdateArticle(article Article) (ra int64, err error) {
 //传入参数：一个文章id
 //传出参数：Article表中影响的数据行数，错误信息
 func DeleteArticle(id int) (ra int64, err error) {
-	stmt, err := db.SqlDb.Prepare("DELETE FROM Article WHERE Id=?")
+	// SqlDb := db.Init()
+	SqlDb, err := sql.Open("mysql", "root:1996@tcp(127.0.0.1:3306)/blog")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer SqlDb.Close()
+	stmt, err := SqlDb.Prepare("DELETE FROM Article WHERE Id=?")
 	if err != nil {
 		log.Fatalln(err)
 	}
 	//执行删除
-	res, err := stmt.Exec(1)
+	res, err := stmt.Exec(id)
 	if err != nil {
 		log.Fatalln(err)
 	}
